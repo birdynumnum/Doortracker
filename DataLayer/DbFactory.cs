3848c80e18032be4bfc89140ad71605e3e65cc68
birdynumnum <f.dersjant@quicknet.nl>
2016-11-25 17:04:59 +0100

    first

---
+++ DataLayer/DbFactory.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DataLayer
+{
+    public class DbFactory : IDbFactory
+    {
+        //TODO: implement IDisposable
+        private DoorContext context;
+
+        public DoorContext Init()
+        {
+            return context ?? (context = new DoorContext());
+        }
+    }
+}
